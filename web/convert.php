e3793e136a6501a2d359a08d9e58369bc95297d3
Kamil Kamiński <kamil@programistyk.pl>
2015-07-26 14:52:27 +0200

    * A minimal error handler for convert.php

--- web/convert.php
+++ web/convert.php
@@ -2,25 +2,30 @@
 
 require_once 'bootstrap.php';
 
-$provider = new FuelioImporter\ConverterProvider();
+try {
+    $provider = new FuelioImporter\ConverterProvider();
 
-$converter = $provider->get(@$_POST['c']);
+    $converter = $provider->get(@$_POST['c']);
 
 // Check if we've got any file
-if (!isset($_FILES['f']))
-    throw new FuelioImporter\NoFileUploadedException();
+    if (!isset($_FILES['f']))
+        throw new FuelioImporter\NoFileUploadedException();
 
-$file = &$_FILES['f'];
+    $file = &$_FILES['f'];
 
-if ($file['error'])
-    throw new FuelioImporter\UploadError($file);
+    if ($file['error'])
+        throw new FuelioImporter\UploadError($file);
 
-$converter->setCarName($_POST['n']);
-$fname = 'FuelioBackup-' . ucfirst(preg_replace('/\s+/', '-', $converter->getTitle())) . '.csv';
+    $converter->setCarName($_POST['n']);
+    $fname = 'FuelioBackup-' . ucfirst(preg_replace('/\s+/', '-', $converter->getTitle())) . '.csv';
 
 //header('Content-Type: text/plain, charset=UTF-8');
-header('Content-Type: text/csv, charset=UTF-8');
-header('Content-Disposition: attachment; filename="'.$fname.'"');
-$outfile = $converter->processFile(new SplFileObject($file['tmp_name']));
-$outfile->rewind();
-$outfile->fpassthru();
+    header('Content-Type: text/csv, charset=UTF-8');
+    header('Content-Disposition: attachment; filename="' . $fname . '"');
+    $outfile = $converter->processFile(new SplFileObject($file['tmp_name']));
+    $outfile->rewind();
+    $outfile->fpassthru();
+} catch (\Exception $ex) {
+    header('Content-Type: text/plain, charset=UTF-8', true, 500);
+    die('An unknown error occured.');
+}
